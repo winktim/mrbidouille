3ec6d91988c8aea5ddcf2e814ee3c948e096d762
winktim <tfs.winkelmann@gmail.com>
2020-03-29 19:24:46 +0200

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,30 @@ export default {
       },
       { hid: 'theme-color', name: 'theme-color', content: '#212121' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    ],
     script: [
       {
         src: `https://polyfill.io/v3/polyfill.min.js?features=${features}`,
