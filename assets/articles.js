99151f329115f096ec27b3cc116c2ae95816289a
winktim <tfs.winkelmann@gmail.com>
2020-04-14 17:14:21 +0200

    Added first article Removed test article

--- assets/articles.js
+++ assets/articles.js
@@ -6,7 +6,7 @@ const articles = {
       date: '2020-04-01',
       tags: ['en', 'opinions'],
       img: '/images/articles/2020-04-01-introduction/is200.jpg',
-      title: 'Who are you and what am I doing here ?',
+      title: 'Who are you and what am I doing here',
       summary:
         'This article has for goal to introduce who "Mr. Bidouille" is, and why you might want to bookmark this blog...',
       link: '/en/2020-04-01-introduction',
@@ -18,7 +18,7 @@ const articles = {
       date: '2020-04-01',
       tags: ['fr', 'opinions'],
       img: '/images/articles/2020-04-01-introduction/is200.jpg',
-      title: "Qui êtes-vous et qu'est-ce que je fais ici ?",
+      title: "Qui êtes-vous et qu'est-ce que je fais ici",
       summary:
         'Cet article a pour but de présenter qui est "Mr. Bidouille", et pourquoi vous voudriez garder un oeil sur ce blog...',
       link: '/fr/2020-04-01-introduction',
---
+++ pages/en/2020-04-01-introduction.vue
@@ -0,0 +1,223 @@
+<template>
+  <article-shell
+    :date="article.date"
+    :tags="article.tags"
+    :img="article.img"
+    :title="article.title"
+    :summary="article.summary"
+    :link="article.link"
+    :hyvorId="article.hyvorId"
+  >
+    <p>
+      Or is it the other way around? I don’t know anymore. But the development
+      of this blog is finally over and I can start writing stuff! And to start
+      things up, let me tell you a few things about myself, to try and make the
+      rest of the blog more relatable.
+    </p>
+    <p><br /></p>
+    <p>
+      English is an easy language. Sort of. At least, it is easier than French,
+      because you don’t have to wonder how to address someone when writing. You
+      can just say “you”. In French, there is a casual and a non casual way to
+      say “you”, and you always have to choose which one you want to use.
+    </p>
+    <p><br /></p>
+    <p>
+      <span
+        >Because yes, if you hadn't noticed by now, this blog is actually </span
+      ><span>bilingual. </span
+      ><span
+        >What it means is the interface is in English, because to me, it is an
+        international language, and the articles are w</span
+      ><span>ritten</span
+      ><span>
+        in the two languages that I know, which are French and English.</span
+      >
+    </p>
+    <p>
+      <span>And when I say “know”, </span
+      ><span>don’t get me wrong! I don’t actually pretend to know e</span
+      ><span>ither</span
+      ><span>
+        of those languages! I’ll probably make plenty of mistakes, some funnier
+        than others.</span
+      >
+    </p>
+    <p>
+      <span
+        >Because yeah, I’m Swiss. That means my primary language is not English,
+        but I learned it in school (naah, mostly on the internet). So I might
+        not actually use the language like it was meant to be, and I might be
+        i</span
+      ><span>nfluenced</span
+      ><span>
+        by American English, Australian English, Japanese Enlgish (wtf?)…</span
+      >
+    </p>
+    <p><br /></p>
+    <p>
+      <span>Well then to the point. I’m a 20 years old Swiss c</span
+      ><span>heese</span
+      ><span>
+        that likes tech. More recently, since I got my driving license 2 years
+        ago (yeah man, got it at 18, you know how it is). I know I just ended a
+        sentence that wasn’t finished, but I need to make a little
+        digression.</span
+      >
+    </p>
+    <p><br /></p>
+    <p>
+      I speak (and will always speak) in a very chill manner, almost second
+      degree, with old and stupid references and will often parody specific ways
+      of talking. That’s just how it is. You’ll have to get used to it. I hope
+      you’ll come to like reading me (I don’t think you will).
+    </p>
+    <p><br /></p>
+    <p>
+      Well, now that I got my way of speaking out of the way, we can get on our
+      way. (I tried to translate the stupid stuff
+      <nuxt-link to="/fr/2020-04-01-introduction">I said in French</nuxt-link>,
+      but I’m afraid I can’t come up with the same level of language shitery in
+      English). I’ve been getting into cars in the last 2 years. Half of it was
+      due to the Japanese anime Initial D and the other half, the simple fact
+      that I had a driving license and a car at my disposal.
+    </p>
+    <p><br /></p>
+    <p>
+      That car in question was an Audi A3 from 1997. I don’t really know much of
+      it’s features other than it was really slammed, because at the time, I
+      didn’t know anything about cars (since then I looked it up, and I would
+      say it was a nice first car to have).
+    </p>
+    <figure>
+      <a href="/images/articles/2020-04-01-introduction/audi.jpg"
+        ><img
+          src="/images/articles/2020-04-01-introduction/audi.jpg"
+          alt="Picture of the Audi A3"
+      /></a>
+      <span>
+        One of the rare pictures of the Audi A3. RIP (yeah she got scrapped)
+      </span>
+    </figure>
+    <p>
+      <span>I first got </span><span>interested</span><span> i</span
+      ><span>n </span
+      ><span
+        >performance driving. Then, (ironically just like Takumi in Initial D) I
+        realized that knowing what was happening under the hood would be of
+        great help to keep going forward in this domain.</span
+      >
+    </p>
+    <p>
+      I quickly realized the Audi was a front wheel drive (duuh), and I
+      obviously wanted a rear wheel drive. Drifts looked quite cool, you know
+      what I’m saying. After ripping the Audi (a little), which was actually my
+      parents car (haha), I decided to by my very own car.
+    </p>
+    <p><br /></p>
+    <p>
+      It took a few months, and I finally settled on a Lexus IS200 from 2000.
+    </p>
+    <p>
+      This car is incredible. You can find so many of them second hand in
+      Switzerland in very good shape from 1’000 to 5’000 swiss francs, and she
+      checks all my criteria:
+    </p>
+    <ul>
+      <li>Rear wheel drive</li>
+      <li>Japanese</li>
+      <li>90’ car (designed in 1998)</li>
+      <li>At least a 4 seater (she’s a 5)</li>
+    </ul>
+
+    <p>
+      She’s a fabulous car, and you will get to know her along side me on this
+      blog. I’m trying to know everything about her, but there’s still much to
+      learn.
+    </p>
+    <figure>
+      <a href="/images/articles/2020-04-01-introduction/is200.jpg"
+        ><img
+          src="/images/articles/2020-04-01-introduction/is200.jpg"
+          alt="Photo de la Lexus IS200"
+      /></a>
+      <span>
+        La Lexus aux abords de son habitat naturel: le Touge Suisse.
+      </span>
+    </figure>
+    <p>
+      I’ve had it for a few months now, and lots happened already. Now she just
+      really needs a good old complete maintenance. And I decided to do
+      everything on her myself, even though my knowledge is quite limited at the
+      moment. It will force me to learn. So I hope you’ll forgive me when I’ll
+      do weird shit, and when I’ll use the wrong terminologies for things.
+    </p>
+    <p><br /></p>
+    <p>
+      So yeah, there you have it. This blog will showcase my adventures
+      servicing my car, replacing pieces (of course I have a few mods planned.
+      I’ll talk about it in a future article), how I get my hands on parts here
+      in Switzerland, etc.
+    </p>
+    <p><br /></p>
+    <p>
+      But I’ll also talk about a few projects related to car simulators, and
+      other mods and addons for cars that I want to create.
+    </p>
+    <p>
+      <span
+        >I’ll also be selling stuff. But wait! Not some stupid merch (for now),
+        I’ll develop kits and other useful things for my car and maybe other
+        cars in the future. You’ll hear more in the coming months. I’m currently
+        in the process of testing my first kit, which I hope will be useful
+        z</span
+      ><span>o</span
+      ><span>
+        many people. As a teaser, I’m working on the problem of the slow
+        electric throttle body of the IS200. I won’t say more for now!</span
+      >
+    </p>
+    <p><br /></p>
+    <p>
+      This is also my very first time blogging. I hope you will find it
+      interesting.
+    </p>
+    <p>
+      Don’t hesitate to use the comments below to give me feedback. You can
+      event comment without an account!
+    </p>
+    <p>
+      Otherwise, you can find my email address at the bottom of the page, if you
+      want a private conversation.
+    </p>
+    <p>
+      I’ll also create a Youtube channel very soon, but I won’t be making full
+      on videos. The channel will contain short clips used in my articles.
+    </p>
+    <p><br /></p>
+    <p>
+      On that note, I’ll end this first article. I’ll see you soon!
+    </p>
+    <p><br /></p>
+    <p>
+      Bidouille
+      <span class="text-sm text-gray-300"
+        >(It’s my username. If you have no idea what it means, check out
+        <nuxt-link to="/about">the “About” page</nuxt-link>)</span
+      >
+    </p>
+  </article-shell>
+</template>
+<script>
+import ArticleShell from '../../components/articleshell'
+import articles from '../../assets/articles'
+
+export default {
+  components: { ArticleShell },
+  data() {
+    return {
+      article: articles.en['2020-04-01-introduction'],
+    }
+  },
+}
+</script>
